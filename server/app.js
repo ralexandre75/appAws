93d1d1e91270f7e8ec6459f35e02bc62e2c9d860
ralexandre75 <rene.alexandre@grimoire-design.fr>
2019-01-28 18:15:54 +0100

    installation de mongodb et mongoose

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose');
 
 var app = express();
 
@@ -10,6 +11,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+
+mongoose.connect('mongodb://ralexandreessaiaws:Saj9VrY2dz2XWjGkPqUm@ds141654.mlab.com:41654/essaiaws', {}, (err) => {
+  if(err) {
+    console.log(err);
+  } else {
+    console.log("Connexion DB OK !")
+  }
+} )
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'))
